ddff6fec95cb85a8af0152d11a9b9d58fbc9208d
Judson Paiva <judson.paiva@hotmail.com>
2021-07-29 03:15:12 +0100

    adicao da classe analista

---
+++ src/main/kotlin/banco/Analista.kt
@@ -0,0 +1,9 @@
+package banco
+
+class Analista(
+    nome: String,
+    cpf: String,
+    salario: Double): Funcionario(nome, cpf, salario) {
+
+    override fun calculoAuxilio(): Double = salario * 0.1
+}
