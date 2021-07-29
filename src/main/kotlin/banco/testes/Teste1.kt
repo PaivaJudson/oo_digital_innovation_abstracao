8a47c25772b87bb3b049794397b643683279b438
Judson Paiva <judson.paiva@hotmail.com>
2021-07-29 07:22:08 +0100

    trabalhando com Polimorfismo

--- src/main/kotlin/banco/testes/Teste1.kt
+++ src/main/kotlin/banco/testes/Teste1.kt
@@ -1,12 +1,21 @@
 package banco.testes
 
+import banco.Analista
 import banco.Funcionario
-import banco.Pessoa
+import banco.Gerente
+
 
 fun main() {
     //val p1 = Pessoa("Quissanga", "123")
 
-    val f1 = Funcionario("Judson", "123.123-11", 3000000.0)
+    val f1 = Analista("Judson Paiva", "123.123-11", 3000000.0)
+    val f2 = Gerente ("Leonor José", "123.123-11", 3000000.0)
 
+
+
+    println(imprimirRelatorio(f1) + "\n\n")
+    println(imprimirRelatorio(f2))
 
 }
+
+fun imprimirRelatorio(f: Funcionario): String = f.toString()
